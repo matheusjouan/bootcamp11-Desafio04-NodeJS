2d0ae442d33993dce6e7bfade3cbec1f5b1b62e3
Matheus Jouan <matheusjouan@hotmail.com>
2020-06-19 16:17:14 -0300

    Finished the challenge

---
+++ README.md
@@ -0,0 +1,25 @@
+# bootcamp11-Desafio04-NodeJS
+
+Aplicar os conceitos de NodeJS e TypeScript, criando uma aplicação que deve armazenar transações financeiras de entrada e saída, que deve permitir o cadastro e a listagem dessas transações.
+A partir da aplicação, podemos fazer entradas e saídas (movimentação financeira) respeitando algumas RN´s definida no projeto. Também conterá uma rota para trazer um resumo de todas as movimentações, e com um resumo das saidas, entrada e total dentro da conta.
+
+# Procedimentos para Funcionamento
+
+## Instalando as Dependências
+
+Após de baixar o repositório, deve-se executar o comando abaixo, no qual será baixado todas as libs que estão sendo utilizado no projeto.
+
+> yarn
+
+## Executando o Projeto
+
+Para executar o projeto, digite o comando abaixo, assim o server do backend será startado:
+
+> yarn dev:server
+
+# Ferramentas utilizadas
+
+- express: framework do NodeJS para microserviços
+- uuidv4: gera de forma uma string única, que será usado para id.
+- typescript: o qual fornece o recurso de tipagem de variáveis, funções, classes.
+- ESLint, Pretter, EditorConfig: ferramentas para padronização do código, que auxilia no desenvolvimento de aplicação.
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc: Balance, current: TransactionDTO) => {
+        if (current.type === 'income') {
+          acc.income += current.value;
+        } else if (current.type === 'outcome') {
+          acc.outcome += current.value;
+        }
+        return {
+          income: acc.income,
+          outcome: acc.outcome,
+          total: acc.income - acc.outcome,
+        };
+      },
+      {
+        outcome: 0,
+        income: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
